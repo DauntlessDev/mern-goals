72ccd871bde07197317fa909c04f6250a4696403
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-03-01 10:31:30 +0800

    added loginUser code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,7 +14,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please add all fields");
   }
 
-  // Check if user exists
+  // Check if user exists1
   const userExists = await User.findOne({ email });
 
   if (userExists) {
@@ -49,7 +49,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route      POST /api/users/login
 // @access     )PubliasyncHandler(c
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user credentials");
+  }
 });
 
 // @desc       Get user data
