4f8bc49f0816ab554910a8afd544585ee17e9e95
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-03-11 12:32:09 +0800

    added frontend for login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       </div>
       <ul>
         <li>
-          <Link to="/">
+          <Link to="/login">
             <FaSignInAlt /> Login
           </Link>
         </li>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,67 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
 function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password2: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="Heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please sign-in your account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              name="form-control"
+              id="email"
+              // name="name"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="form-control"
+              id="password"
+              // name="name"
+              value={password}
+              placeholder="Enter passsword"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Login;
